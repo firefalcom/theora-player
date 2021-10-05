e2454da3e4035b6b83ab82fa88fa7e9a93efd99a
Gauthier Billot <gogoprog@gmail.com>
2021-10-05 20:31:33 +0200

    Wip

--- include/theora-player.h
+++ include/theora-player.h
@@ -2,4 +2,6 @@
 
 namespace theoraplayer {
 
+    void test();
+
 }
--- test/main.cpp
+++ test/main.cpp
@@ -2,16 +2,22 @@
 
 #include <SDL2/SDL.h>
 
-SDL_Window *window;
-SDL_Surface *surface;
+/* SDL_Window *window; */
+/* SDL_Surface *surface; */
 
 int main()
 {
     puts( "theora-player-test" );
 
-    SDL_Init( SDL_INIT_VIDEO );
-    window = SDL_CreateWindow( "theora-player-test", 0, 0, 640, 480, 0 );
-    surface = SDL_GetWindowSurface( window );
+    theoraplayer::test();
+
+    /* SDL_Init( SDL_INIT_VIDEO ); */
+    /* window = SDL_CreateWindow( "theora-player-test", 0, 0, 640, 480, 0 ); */
+    /* surface = SDL_GetWindowSurface( window ); */
+
+    /* SDL_Delay( 2000 ); */
+
+    /* SDL_Quit(); */
 
     return 0;
 }
