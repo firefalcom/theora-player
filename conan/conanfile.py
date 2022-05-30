c462d3c2599f0d9a60788b4e2bdb59476371a13a
Gauthier Billot <gogoprog@gmail.com>
2022-05-30 17:29:42 +0200

    Add conan recipe (#7)

---
+++ conan/conanfile.py
@@ -0,0 +1,34 @@
+from conans import ConanFile, CMake, tools
+
+required_conan_version = ">=1.33.0"
+
+class TheoraPlayerConan(ConanFile):
+    name = "theora-player"
+    url = "https://github.com/firefalcom/conan-theora-player"
+    homepage = "https://github.com/firefalcom/theora-player"
+    description = "Embeddable Theora Player"
+    topics = "video", "embeddable", "player"
+    license = "MIT"
+
+    settings = "os", "arch", "compiler", "build_type"
+    options = {}
+    default_options = {}
+
+    exports_sources = "CMakeLists.txt", "../src/*", "../include/*"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def requirements(self):
+        self.requires("ogg/1.3.5")
+        self.requires("theora/1.1.1")
+        self.requires("vorbis/1.3.7")
+
+    def package(self):
+        self.copy("*.h", dst="include")
+        self.copy("*.a", dst="lib")
+
+    def package_info(self):
+        self.cpp_info.libs = ["theora-player"]
